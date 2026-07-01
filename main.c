496ee1be7a0feb867c3499bc3afcc618f11fc626
Miguel Assis Silva <miguelassissilva@gmail.com>
2026-07-01 14:48:47 -0300

    - implementação de funcionalidade para caracteres especiais em windows

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include "algebra.h"
 
+/*bloco condicional de pré-processamento (prévia a compilação), para verificar
+  se está compilando em sistema Windows. Se estiver inclui a biblioteca
+  windows.h */
+
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
 int main () {
+
+    /*verifica mais uma vez se está no windows, e se estiver chama duas funções
+      que resolvem o problema de caracteres especiais no terminal windows. */
 
     #ifdef _WIN32
         SetConsoleOutputCP(CP_UTF8);
